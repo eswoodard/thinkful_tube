da98701604422ab83d7911abf642b2b913f2597d
Elizabeth Woodard <eswoodard75@gmail.com>
2018-06-02 12:10:49 -0400

    prev and next links added

--- index.js
+++ index.js
@@ -6,15 +6,19 @@
 
 const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 
-function getDataFromApi (searchTerm, callback){
+function getDataFromApi (searchTerm, callback, newPageToken){
 	const query = {
 		key: 'AIzaSyDjuwzuBuJAjl-OJQLx8v-ape62xhu_LjI',
 		part: 'snippet',
-		type: 'video',
 		q: `${searchTerm} in:name`,
 		
 	}
+	if (newPageToken) {
+		query.pageToken = newPageToken;
+	}
+
 	$.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+
 }
 
 function renderResult(result) {
@@ -25,7 +29,7 @@ function renderResult(result) {
 			</div>
 			<div class="results-video">
 				<a href="https://www.youtube.com/watch?v=${result.id.videoId}"> <img class = 'js-result-thumbnail' src = "${result.snippet.thumbnails.medium.url}"></a>
-				<br><a href= "https://www.youtube.com/channel/${result.snippet.channelId}">More from ${result.snippet.channelTitle}</a>
+				<br><a href= "https://www.youtube.com/channel/${result.snippet.channelId}">More from ${result.snippet.channelTitle}</a></
 			</div>
 		</div>`;
 }
@@ -34,19 +38,90 @@ function displayYouTubeSearchData(data){
 	const  results = data.items.map((item, index) => 
 	renderResult(item));
 	$('.js-search-results').html(results);
+	let link = [];
+	if(data.prevPageToken){
+		link.push(renderPreviousLink(data.prevPageToken));
+		console.log("renderPreviousLink ran")};
+	link.push(renderNextLink(data.nextPageToken));
+		
+	
+	
+	displayLink(link);
 	
 }
+
+function renderPreviousLink(token) {
+	return`
+	
+		<div class = 'js-prev-link'>
+			<a href="#" class="previous" token="${token}">< Previous</a>
+		</div>
+	`
+
+}
+
+function renderNextLink(token) {
+	return`
+		
+		<div class = 'js-next-link'>
+			<a href="#" class="next" token="${token}">Next ></a>
+		</div>
+	`
+}
+
+
+
+function displayLink(link) {
+	console.log("displayLink Ran");
+	$('.js-search-results').append(link);
+	console.log(link);
+
+}
+
+function handleNextLink() {
+	console.log('handleNextLink ran');
+	$('.js-search-results').on('click', '.next', event => {
+		event.preventDefault();
+		const queryTarget = $('.js-query');
+		const query = queryTarget.val();
+		const token = $(".next").attr('token')
+		getDataFromApi(query, displayYouTubeSearchData, token);
+		console.log();
+	});
+
+}
+
+function handlePrevLink() {
+	console.log('handlePrevLink ran');
+	$('.js-search-results').on('click', '.previous', event => {
+		event.preventDefault();
+		const queryTarget = $('.js-query');
+		const query = queryTarget.val();
+		const token = $(".previous").attr('token')
+		getDataFromApi(query, displayYouTubeSearchData, token);
+		console.log();
+	});
+
+}
+
 
 function watchSubmit() {
 	$('.js-search-form').submit(event => {
 		event.preventDefault();
 		const queryTarget = $(event.currentTarget).find('.js-query');
 		const query = queryTarget.val();
-		queryTarget.val("");
+		//queryTarget.val("");
 		getDataFromApi(query, displayYouTubeSearchData);
+
 		
 		
 	});
+
+	handleNextLink();
+	handlePrevLink();
+	
 }
 
-$(watchSubmit);
+watchSubmit();
+
+
